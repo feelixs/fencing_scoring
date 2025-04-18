fb0b84f90e785c37d67e4bdfed9f539225728ce3
Michael Felix <mikemh@uri.edu>
2025-04-17 22:14:19 -0400

    revert to original

--- create_sounds.py
+++ create_sounds.py
@@ -2,7 +2,7 @@ from pydub import AudioSegment
 from pydub.playback import play
 
 # Load original sound
-sound = AudioSegment.from_mp3("sounds/fencing_beep.mp3")
+sound = AudioSegment.from_mp3("sounds/defeat.mp3")
 
 # Create higher pitch version (increase by 3 semitones)
 higher_pitch = sound._spawn(sound.raw_data, overrides={
